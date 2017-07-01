e87bb6c2ba02cbff42ffa545c5cb8d78e20c3e94
BlisS <contacto@fixter.org>
2017-07-01 16:15:32 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
+import NavBar from './common/NavBar';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav></nav>
-        <Routes />
-      </div>
+<Layout>
+    <NavBar />
+    <Content style={{ padding: '0 50px' }}>
+      <Routes />
+    <Footer style={{ textAlign: 'center' }}>
+        Mundi  ©2017 Created by <a href="http://fixter.org">
+            Fixter.org
+        </a>
+    </Footer>
+  </Layout>
     );
   }
 }
